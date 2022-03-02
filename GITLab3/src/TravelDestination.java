5e8efc1a5e57f1fbea108e224e0259a192a8dfd1
Edward Morales III <49822111+edm0ra@users.noreply.github.com>
2022-03-01 20:25:59 -0600

    My first commit.

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -28,7 +28,7 @@ public class TravelDestination {
 				System.out.println("My name is Ernesto Palacios. I wish to travel to Venice one day!");
 		    		System.out.println("My name is Opeyemi Ayeni. I wish to travel to Japan one day!");
 				System.out.println("My name is Lissette De Leon, I wish to travel to Mars one day !");
-
+				System.out.println("My name is Eddie Morales. I wish to travel to Canada one day !");
 	}
 
 }
